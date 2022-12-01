63ee80da14052d2ca88cad4ef081227443ff8455
Michael Debono Mrđen <michael@debonomren.eu>
2022-12-01 22:55:00 +0100

    Day 1 Part 2

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -2,13 +2,15 @@ f = open("2022/01/input.txt", "r")
 
 sum = 0
 largest = 0
+list = []
 
 for line in f:
     line = line.strip()
     if (line == ''):
         if sum > largest:
             largest = sum
-            print('largest = ', largest)
+            print('largest =', largest)
+        list.append(sum)
         sum = 0
     else:
         x = int(line)
@@ -16,5 +18,11 @@ for line in f:
 
 if sum > largest:
     largest = sum
+list.append(sum)
 
-print('final largest = ', largest)
+print('final largest =', largest)
+
+list.sort(reverse=True)
+print(list[0:3])
+total = list[0] + list[1] + list[2]
+print('total =', total)
