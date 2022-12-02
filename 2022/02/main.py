d29d2b73fc0d1cea250c18b1f511023f2ee9fa4d
Michael Debono Mrđen <michael.debono@gmail.com>
2022-12-02 06:58:53 +0000

    Day 2 add functions

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -3,4 +3,11 @@ f = open("2022/02/smallinput.txt")
 for line in f:
     line = line.strip()
     print(line)
+    print(get1(line), get2(line))
     
+    
+def get1(line):
+    return line.split(' ')[0]
+
+def get2(line):
+    return line.split(' ')[1]
