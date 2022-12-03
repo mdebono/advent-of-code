46f625dd29fdf0aa29ba0a1e46e778c591bf2563
Michael Debono Mrđen <michael@debonomren.eu>
2022-12-03 11:29:01 +0100

    Day 2 part 1

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -1,13 +1,36 @@
-def get1(line):
-    return line.split(' ')[0]
+def tokenize(line):
+    array = line.strip().split(' ')
+    return array[0], array[1]
 
-def get2(line):
-    return line.split(' ')[1]
+def shape(shape):
+    if shape in ['A', 'X']:
+        return 1
+    if shape in ['B', 'Y']:
+        return 2
+    if shape in ['C', 'Z']:
+        return 3
+    raise ValueError
 
-f= open("2022/02/smallinput.txt")
+def outcome(other, you):
+    if shape(you) == shape(other):
+        return 3
+    if ((shape(you) == 1 and shape(other) == 3) # Rock defeats Scissors
+        or (shape(you) == 3 and shape(other) == 2) # Scissors defeats Paper
+        or (shape(you) == 2 and shape(other) == 1)): # Paper defeats Rock
+        return 6
+    return 0
 
+def get_score(other, you):
+    return shape(you) + outcome(other, you)
 
+f = open("2022/02/input.txt")
+
+sum = 0
 for line in f:
-    line = line.strip()
-    print(line)
-    print(get1(line), get2(line))
+    line = tokenize(line)
+    other = line[0]
+    you = line[1]
+    score = get_score(other, you)
+    print(other, you, score)
+    sum = sum + score
+print(sum)
